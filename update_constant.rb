49087b6be715640db45b0be7a4e31f42fa7e675b
Jiang Sheng <gisonrg@gmail.com>
2016-11-03 17:55:35 +0800

    add script to save league_id_to_name

--- update_constant.rb
+++ update_constant.rb
@@ -22,6 +22,12 @@ urls = { regions: 'https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota
          items_games: 'https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/items/items_game.json',
          leagues: 'https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/items/leagues.json'}
 
+DOTA2_API_KEY = 'A72DE7D7BE9870C8DA671D67941CCAA7'
+API_BASE_URL = 'https://api.steampowered.com/IDOTA2Match_570'
+API_URLS = {
+  get_league: "#{API_BASE_URL}/GetLeagueListing/v1"
+}
+
 live_url = { uniqueusers: 'http://www.dota2.com/jsfeed/uniqueusers' }
 
 def get_pediadata(type, lang_code)
@@ -32,12 +38,36 @@ def get_pediadata(type, lang_code)
   request_and_save(uri, filepath)
 end
 
+def get_league(lang_code)
+  uri = URI(API_URLS[:get_league])
+  params = {:language=>lang_code, :format=>'json', :key => DOTA2_API_KEY}
+  uri.query = URI.encode_www_form(params)
+  filepath = "locales/#{lang_code}/league_id_to_name.yml"
+  tables = {}
+  data = JSON.parse(request(uri))
+  data['result']['leagues'].each do |league|
+    tables[league['leagueid']] = league['name']
+  end
+  save_json_to_file(filepath, tables)
+end
+
 def get_jsfeed(jsfeed,lang_code)
   uri = URI(JSFEEDS[jsfeed])
   params = {:l=>LANGUAGES[lang_code]}
   uri.query = URI.encode_www_form(params)
   filepath = "locales/#{lang_code}/#{jsfeed}.yml"
   request_and_save(uri, filepath)
+end
+
+def request(uri)
+  response = Net::HTTP.get_response(uri)
+  return response.body
+end
+
+def save_json_to_file(filepath, data)
+  File.open(filepath,'wb') do |file|
+    file.write data.to_yaml
+  end
 end
 
 def request_and_save(uri, filepath)
@@ -58,6 +88,8 @@ LANGUAGES.keys.each do |lang_code|
     get_pediadata(type, lang_code)
   end
 
+  get_league(lang_code)
+
 end
 
 urls.each do |key, url|
@@ -65,4 +97,3 @@ urls.each do |key, url|
   filepath = "yml/#{key}.yml"
   request_and_save(uri, filepath)
 end
-
